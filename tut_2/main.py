7c294fde3480c964327bf9477fed66f35bc3b677
stijnvdelsen <stijnvandenelsen@gmail.com>
2020-03-22 21:12:25 +0100

    Lowered the epoch, because it wasn't giving me a better accuracy and only costs time.

--- tut_2/main.py
+++ tut_2/main.py
@@ -32,7 +32,7 @@ model = keras.Sequential([
 
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
-model.fit(train_images, train_labels, epochs=10)
+model.fit(train_images, train_labels, epochs=8)
 
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 

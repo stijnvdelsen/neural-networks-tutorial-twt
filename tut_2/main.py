d33229f85c7e044f00f5db0cfded9cc59e725729
stijnvdelsen <stijnvandenelsen@gmail.com>
2020-03-22 20:58:50 +0100

    Finished tutorial 3. We created our neural infrastructure and trained and tested our model. We got up to 88% accuracy.

--- tut_2/main.py
+++ tut_2/main.py
@@ -16,7 +16,24 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
+"""
+This will show the image irl
+"""
 #print(train_images[7])
 
 #plt.imshow(train_images[7], cmap=plt.cm.binary)
 #plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+    ])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print("Tested Acc: ", test_acc)
